b62ef57877f0c6dc0e98a4ad369ce14bc1111762
Param Siddharth <contact@paramsid.com>
2022-02-27 13:46:49 +0000

    Variables
    Signed-off-by: Param Siddharth <contact@paramsid.com>

---
+++ 2-variables/Variables.scala
@@ -0,0 +1,17 @@
+object Variables extends App {
+	// Immutable (Java: final)
+	val a = "Param"				// String
+	val aa: String = "Param"	// String
+	val c = 'a'					// Char
+	val b = 1					// Int
+	val d: Byte		= 1
+	val e: Long		= 2
+	val f: Short	= 3
+	val g: Double	= 4.0
+	val h: Double	= 5.0
+
+	// Mutable
+	var x = 1
+	x = 2
+	x = 3
+}
